290452cdd8cfc4462a1f60ad161ec8979c66e98f
Fernando Acosta <fer.acsvi@gmail.com>
2023-03-13 11:59:09 -0600

    pruebas

--- animalia/wsgi.py
+++ animalia/wsgi.py
@@ -8,13 +8,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-from django.contrib.auth.models import User
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animalia.settings')
 
 application = get_wsgi_application()
-
-users = User.objects.all()
-if not users:
-    User.objects.create_superuser(username="admin", email="user@example.com", password="admin", is_active=True, is_staff=True)
