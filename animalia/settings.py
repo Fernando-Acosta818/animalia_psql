e55e4c34a6a42dac05beede0e39e9bdb5674717e
Fernando Acosta <fer.acsvi@gmail.com>
2023-03-13 11:08:13 -0600

    pruebas db

--- animalia/settings.py
+++ animalia/settings.py
@@ -83,10 +83,9 @@ WSGI_APPLICATION = 'animalia.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',        
+        conn_max_age=600)
 }
 
 
